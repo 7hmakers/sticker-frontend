a3d09b78104bc2c6e47d2c7a83f39c4ee8fd361d
so1ve <i@mk1.io>
2023-03-08 13:39:03 +0800

    fix: path

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,10 @@ import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import UnoCSS from "unocss/vite";
+import Legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
+  base: "/sticker",
   plugins: [
     Vue(),
     AutoImport({
@@ -22,6 +24,10 @@ export default defineConfig({
       dts: "src/components.d.ts",
     }),
     UnoCSS(),
+    Legacy({
+      targets: ["defaults", "IE 11"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+    }),
   ],
   resolve: {
     alias: {
